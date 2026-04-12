b2b230657b21793e051a764ce5b678bf92c238ed
nmanclank <7421094+nmanclank@users.noreply.github.com>
2026-04-11 22:14:28 -0700

    Messing around with drawing colors in window surface.

--- iv.c
+++ iv.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <SDL2/SDL.h>
 
-int main(int argc, char *argv[]) // <--- Add these arguments
+int main(int argc, char *argv[]) 
 {
     printf("Hello, World!\n");
     
-    // Initialize SDL (Mandatory for the window to actually appear)
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
@@ -15,7 +16,16 @@ int main(int argc, char *argv[]) // <--- Add these arguments
     SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", 
                                            SDL_WINDOWPOS_CENTERED, 
                                            SDL_WINDOWPOS_CENTERED, 
-                                           800, 600, 0);
+                                           800, 600, 0); // Create the actual window 
+
+    SDL_Surface *psurface = SDL_GetWindowSurface(pwindow); // Getting the surface inside window
+    SDL_FillRect(psurface, NULL, 0x000000FF); // Fill the "Surface" with the color blue
+    SDL_UpdateWindowSurface(pwindow); // Update the surface inside of our window
+    SDL_Delay(3000); 
+    SDL_FillRect(psurface, NULL, 0x00FF0000); // Fill the "Surface" with the color red
+    SDL_UpdateWindowSurface(pwindow); // Update the surface inside of our window
+
+
 
     SDL_Delay(3000); 
 }
