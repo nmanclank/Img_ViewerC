221bf6e0be72a9cd4f13b9bf5faa92a2fd01c591
nmanclank <7421094+nmanclank@users.noreply.github.com>
2026-04-13 09:21:57 -0700

    Target pixels when rendering

--- iv.c
+++ iv.c
@@ -5,7 +5,9 @@
 
 int main(int argc, char *argv[]) 
 {
-    printf("Hello, World!\n");
+    // printf("Hello, World!\n");
+    const int width = 900;
+    const int height = 600;
     
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -19,13 +21,29 @@ int main(int argc, char *argv[])
                                            800, 600, 0); // Create the actual window 
 
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow); // Getting the surface inside window
-    SDL_FillRect(psurface, NULL, 0x000000FF); // Fill the "Surface" with the color blue
+
+    Uint8 r,g,b;
+    r=0x1A;
+    g = b = 0;
+
+    Uint32 color = SDL_MapRGB(psurface->format,r,g,b);
+
+    // int x=50, y=50; // define xy for pixel to draw
+    SDL_Rect pixel = (SDL_Rect){0,0,1,1};// Create a single pixel of xy 1x1
+    for(int x = 0; x < width; x++) \
+    {
+        for(int y = 0; y < height; y++) 
+        {
+                pixel.x = x;
+                pixel.y = y;
+                SDL_FillRect(psurface, &pixel, color); // Fill the "Surface" with  color 
+
+        }
+    }
+    // SDL_Rect pixel =(SDL_Rect){x,y,1,1}; // Create a single pixel of xy 1x1
+    // SDL_FillRect(psurface, &pixel, color); // Fill the "Surface" with  color 
     SDL_UpdateWindowSurface(pwindow); // Update the surface inside of our window
     SDL_Delay(3000); 
-    SDL_FillRect(psurface, NULL, 0x00FF0000); // Fill the "Surface" with the color red
-    SDL_UpdateWindowSurface(pwindow); // Update the surface inside of our window
 
 
-
-    SDL_Delay(3000); 
 }
